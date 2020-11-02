b26e49238699db3cd83101a99d4766aba4f2f192
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-11-02 15:04:32 +0000

    Until exercise h) all done

--- run.sh
+++ run.sh
@@ -9,12 +9,37 @@ done
 
 
 # Exercise 2
+
 # e)
-fstconcat compiled/horas.fst compiled/e_to_dots.fst > compiled/horas_e_dots.fst
-fstconcat compiled/horas_e_dots.fst compiled/minutos.fst > compiled/text2num_aux.fst
+fstconcat compiled/horas.fst compiled/e_to_dots.fst > compiled/horas_e_dots_aux.fst
+fstconcat compiled/horas_e_dots_aux.fst compiled/minutos.fst > compiled/text2num_aux.fst
 
 fstrmepsilon compiled/text2num_aux.fst > compiled/text2num.fst
 
+# f)
+fstconcat compiled/horas.fst compiled/eps_to_dots_hours.fst > compiled/lazy2num_aux.fst
+
+fstrmepsilon compiled/lazy2num_aux.fst > compiled/lazy2num.fst
+
+# g)
+fstunion compiled/quartos.fst compiled/meias.fst > compiled/meias_quartos_aux.fst
+fstconcat compiled/hora_to_hora.fst compiled/meias_quartos_aux.fst > compiled/rich2text_aux.fst
+
+fstrmepsilon compiled/rich2text_aux.fst > compiled/rich2text.fst
+
+# h)
+fstconcat compiled/rich2text.fst compiled/text2num.fst > compiled/rich2num_1_aux.fst
+fstunion  compiled/rich2num_1_aux.fst   compiled/lazy2num.fst > compiled/rich2num_2_aux.fst
+fstunion  compiled/rich2num_2_aux.fst   compiled/text2num.fst > compiled/rich2num_3_aux.fst
+
+fstrmepsilon compiled/rich2num_3_aux.fst > compiled/rich2num.fst
+
+# i)
+
+for i in compiled/*_aux.fst; do
+	echo "Removing aux file: compiled/$(basename $i '.fst')"
+    rm $i
+done
 
 for i in compiled/*.fst; do
 	echo "Creating image: images/$(basename $i '.fst').pdf"
