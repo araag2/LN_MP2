a34e89970f3fcf28dfb6a41d7ab19c36514d6b5e
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-11-06 23:03:42 +0000

    Fixed some bugs and made more tests

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 mkdir -p compiled images
 
-for i in tests/horas/*txt tests/minutos/*.txt; do
+for i in tests/horas/*txt tests/minutos/*.txt tests/meias/*.txt tests/quartos/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
@@ -16,15 +16,26 @@ done
 #done
 
 # minutos .txt
-echo "minutos.txt Tests"
-for i in compiled/test_minutos_*.fst; do
-    echo "Testing the transducer 'minutos' with the input compiled/$(basename $i '.fst')"  
-    fstcompose $i compiled/minutos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#echo "minutos.txt Tests"
+#for i in compiled/test_minutos_*.fst; do
+    #echo "Testing the transducer 'minutos' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/minutos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+#echo "meias.txt Tests"
+#for i in compiled/test_meias_*.fst; do
+    #echo "Testing the transducer 'meias' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/meias.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+echo "quartos.txt Tests"
+for i in compiled/test_quartos_*.fst; do
+    echo "Testing the transducer 'quartos' with the input compiled/$(basename $i '.fst')"  
+    fstcompose $i compiled/quartos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 done
 
-
 for i in compiled/test_*.fst; do
-	echo "Removing test file: compiled/$(basename $i '.fst')"
+	#echo "Removing test file: compiled/$(basename $i '.fst')"
     rm $i
 done
 
