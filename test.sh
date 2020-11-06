33d1c9b251f253f3c7a565640852da3533d654b0
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-11-06 23:27:49 +0000

    New tests and some script changes

--- test.sh
+++ test.sh
@@ -2,7 +2,12 @@
 
 mkdir -p compiled images
 
-for i in tests/horas/*txt tests/minutos/*.txt tests/meias/*.txt tests/quartos/*.txt; do
+#for i in tests/horas/*txt tests/minutos/*.txt tests/meias/*.txt tests/quartos/*.txt; do
+	#echo "Compiling: $i"
+    #fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+#done
+
+for i in tests/text2num/*txt tests/lazy2num/*.txt tests/rich2text/*.txt tests/rich2num/*.txt tests/num2text/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
@@ -28,11 +33,41 @@ done
     #fstcompose $i compiled/meias.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 #done
 
-echo "quartos.txt Tests"
-for i in compiled/test_quartos_*.fst; do
-    echo "Testing the transducer 'quartos' with the input compiled/$(basename $i '.fst')"  
-    fstcompose $i compiled/quartos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#echo "quartos.txt Tests"
+#for i in compiled/test_quartos_*.fst; do
+    #echo "Testing the transducer 'quartos' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/quartos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+echo "text2num.txt Tests"
+for i in compiled/test_text2num_*.fst; do
+    echo "Testing the transducer 'text2num' with the input compiled/$(basename $i '.fst')"  
+    fstcompose $i compiled/text2num.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 done
+
+#echo "lazy2num.txt Tests"
+#for i in compiled/test_lazy2num_*.fst; do
+    #echo "Testing the transducer 'lazy2num' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/lazy2num.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+#echo "rich2text.txt Tests"
+#for i in compiled/test_rich2text_*.fst; do
+    #echo "Testing the transducer 'rich2text' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/rich2text.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+#echo "rich2num.txt Tests"
+#for i in compiled/test_rich2num_*.fst; do
+    #echo "Testing the transducer 'rich2num' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/rich2num.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
+
+#echo "num2text.txt Tests"
+#for i in compiled/test_num2text_*.fst; do
+    #echo "Testing the transducer 'num2text' with the input compiled/$(basename $i '.fst')"  
+    #fstcompose $i compiled/num2text.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+#done
 
 for i in compiled/test_*.fst; do
 	#echo "Removing test file: compiled/$(basename $i '.fst')"
