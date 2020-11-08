47ecefd7077369157df6f6354705f4b99c769620
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-11-08 12:57:52 +0000

    Almost final version

--- run.sh
+++ run.sh
@@ -2,7 +2,12 @@
 
 mkdir -p compiled images
 
-for i in sources/*.txt; do
+for i in compiled/*.fst images/*.pdf; do
+	echo "Removing file: compiled/$(basename $i '.fst')"
+    rm $i
+done
+
+for i in sources/*.txt tests/*.txt; do
 	echo "Compiling: $i"
     fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 done
@@ -11,9 +16,7 @@ done
 
 # e)
 fstconcat compiled/horas.fst compiled/e_to_dots.fst > compiled/horas_e_dots_aux.fst
-fstconcat compiled/horas_e_dots_aux.fst compiled/minutos.fst > compiled/text2num_aux.fst
-
-fstrmepsilon compiled/text2num_aux.fst > compiled/text2num.fst
+fstconcat compiled/horas_e_dots_aux.fst compiled/minutos.fst > compiled/text2num.fst
 
 # f)
 fstconcat compiled/horas.fst compiled/eps_to_dots_horas.fst > compiled/lazy2num_aux.fst
@@ -26,15 +29,15 @@ fstconcat compiled/hora_to_hora.fst compiled/meias_quartos_aux.fst > compiled/ri
 
 fstrmepsilon compiled/rich2text_aux.fst > compiled/rich2text.fst
 
-# h) TODO: CORRECT THIS 
-fstcompose compiled/rich2text.fst compiled/text2num.fst > compiled/rich2num_1_aux.fst
+# h)  
+fstcompose compiled/rich2text.fst compiled/text2num.fst | fstarcsort > compiled/rich2num_1_aux.fst
 fstunion  compiled/rich2num_1_aux.fst   compiled/lazy2num.fst > compiled/rich2num_2_aux.fst
 fstunion  compiled/rich2num_2_aux.fst   compiled/text2num.fst > compiled/rich2num_3_aux.fst
 
 fstrmepsilon compiled/rich2num_3_aux.fst > compiled/rich2num.fst
 
 # i)
-fstinvert compiled/rich2num.fst > compiled/num2text_aux.fst
+fstinvert compiled/text2num.fst > compiled/num2text_aux.fst
 
 fstrmepsilon compiled/num2text_aux.fst > compiled/num2text.fst
 
@@ -43,10 +46,25 @@ for i in compiled/*_aux.fst; do
     rm $i
 done
 
+# TESTS
+
+echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
+fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+fstcompose compiled/numero.fst compiled/converter.fst > compiled/numero_result.fst
+
+echo "Testing the Transducer rich2num"
+for i in compiled/sleepH_*.fst compiled/wakeupH_*.fst; do
+    echo "Testing the transducer 'rich2num' with the input compiled/$(basename $i '.fst')"  
+    fstcompose $i compiled/rich2num.fst > compiled/$(basename $i '.fst')_result.fst
+done
+
+echo "Testing the Transducer num2text"
+for i in compiled/sleepI_*.fst compiled/wakeupI_*.fst; do
+    echo "Testing the transducer 'num2text' with the input compiled/$(basename $i '.fst')"  
+    fstcompose $i compiled/num2text.fst > compiled/$(basename $i '.fst')_result.fst
+done
+
 for i in compiled/*.fst; do
 	echo "Creating image: images/$(basename $i '.fst').pdf"
     fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
 done
-
-# echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
-# fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
